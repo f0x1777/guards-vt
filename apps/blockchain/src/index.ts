b74c2b06f34861301c99762782e72b1400ac0d9e
Nicolas Fernandez <nico.fran.fernandez@gmail.com>
2026-03-27 11:35:09 -0300

    fix: close remaining rootstock contract review findings

--- apps/blockchain/src/index.ts
+++ apps/blockchain/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./manifests.js";
-export * as rootstockSurface from "./rootstock.js";
+export { rootstockSurface } from "./rootstock.js";
 export * as cardanoSurface from "./cardano.js";
 export * as svmSurface from "./svm.js";
 export * as evmSurface from "./evm.js";
